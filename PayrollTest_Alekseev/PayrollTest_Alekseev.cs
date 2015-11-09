6e3232e8f6f2dfb39a85a5b16b2ccaf47e9094c3
Rintaar <Blackwhiter@mail.ru>
2015-11-09 09:53:10 +0300

    need_to_add_employee_transaction

---
+++ PayrollTest_Alekseev/PayrollTest_Alekseev.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll_Alekseev;
+
+
+namespace PayrollTest_Alekseev
+{
+    [TestClass]
+    public class PayrollTest_Alekseev
+    {
+      [TestMethod]
+        public void TestEmployee()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+            Assert.AreEqual("Bob", e.Name);
+            Assert.AreEqual("Home", e.Adress);
+            Assert.AreEqual(empId, e.EmpId);
+        }
+      [TestMethod]
+      public void TestEmployeeToString()
+      {
+          Employee e = new Employee(1, "Bob", "Home");
+          Assert.AreEqual("Emp#: 1  Bob  Home  Paid    by ",e.ToString());          
+      }    
+      
+
+    }
+}
---
+++ Payroll_Alekseev/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Alekseev
+{
+    public class Employee
+    {
+        private readonly int empid;
+        private string name;
+        private string adress;        
+        private PaymentClassification classification;
+        private PaymentSchedule schedule;
+        private PaymentMethod method;
+        private Affliation affliation;
+
+        public Employee(int empid, string name, string adress)
+        {
+            this.empid = empid;
+            this.name = name;
+            this.adress = adress;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Adress
+        {
+            get { return adress; }
+        }
+        public int EmpId
+        {
+            get { return empid; }
+        }
+        public PaymentClassification Classification
+        {
+            get { return classification; }
+            set { classification = value; }
+        }
+        public PaymentSchedule Schedule
+        {
+            get { return schedule; }
+            set { schedule = value; }
+        }
+        public PaymentMethod Method
+        {
+            get { return method; }
+            set { method = value; }
+        }
+        public Affliation Affliation
+        {
+            get { return affliation; }
+            set { affliation = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("Emp#: ").Append(empid).Append("  ");
+            builder.Append(name).Append("  ");
+            builder.Append(adress).Append("  ");
+            builder.Append("Paid ").Append(classification).Append("  ");
+            builder.Append(schedule);
+            builder.Append(" by ").Append(method);
+            return builder.ToString();
+        }
+    }
+    public interface Transaction
+    {
+        void Execute();
+    }
+}
